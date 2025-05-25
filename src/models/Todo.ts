a6561945931471a4d96112c2645483e08fe60c21
unknown <a.shataie2023@gmail.com>
2025-05-25 01:51:40 -0700

    fix: Rename PENDING and COMPLETED to pending and completed to match their DTOs.

--- src/models/Todo.ts
+++ src/models/Todo.ts
@@ -6,8 +6,8 @@ const todoSchema = new Schema(
     title: { type: String, required: true },
     status: {
       type: String,
-      enum: ["COMPLETED", "PENDING"],
-      default: "PENDING",
+      enum: ["completed", "pending"],
+      default: "pending",
     },
   },
   { timestamps: true }
